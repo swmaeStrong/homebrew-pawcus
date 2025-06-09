c4e48a08a7469369e3f42598a487ffd66ae70ad9
gadisom <jeongwon.kim@gmail.com>
2025-06-09 12:28:20 +0900

    Add Pawcus v0.0.1

---
+++ Casks/pawcus.rb
@@ -0,0 +1,11 @@
+cask "pawcus" do
+    version "0.0.1"
+    sha256 "01f506bea64cdff78c6e9ebcc3198e606e296f173658120766e72338dd5634a4"
+  
+    url "https://github.com/swmaeStrong/MVP_Mac/releases/download/v#{version}/Pawcus.zip"
+    name "Pawcus"
+    desc "Developer productivity timer for macOS"
+    homepage "https://github.com/swmaeStrong/MVP_Mac"
+  
+    app "Pawcus.app"
+  end
