c39cf1e1cd62ce088a486ef6ed22455f8bfbcfeb
garden <jeongwon.kim64@gmail.com>
2025-06-19 15:07:07 +0900

    Update pawcus.rb

--- Casks/pawcus.rb
+++ Casks/pawcus.rb
@@ -2,10 +2,10 @@ cask "pawcus" do
   version "0.5"
   sha256 "cb11d694ce723bbe792721d24117a83deb7cc8f82fb6f1aa48638beec30d4e84"
 
-  url "https://github.com/swmaeStrong/MVP_Mac/releases/download/v#{version}/Pawcus.zip"
+  url "https://github.com/swmaeStrong/Pawcus/releases/download/v#{version}/Pawcus.zip"
   name "Pawcus"
   desc "Developer productivity timer and app usage tracker for macOS"
-  homepage "https://github.com/swmaeStrong/MVP_Mac"
+  homepage "https://github.com/swmaeStrong/Pawcus"
 
   livecheck do
     url :url
