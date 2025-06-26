ae9b1049aecbd9d7123120f8ca52cf97d8f8c546
GitHub Action <action@github.com>
2025-06-26 17:30:31 +0000

    Auto-update cask for latest release

--- Casks/pawcus.rb
+++ Casks/pawcus.rb
@@ -1,6 +1,6 @@
 cask "pawcus" do
-  version "0.8.5"
-  sha256 "22658fbb76b4c264fb3a09a19c8e384f0b4ae1077e0a43cd328036a509fe2ed6"
+  version "0.8.6"
+  sha256 "ee0cb0167473d0418e7d9e4fecac057bede2e08245569d269a6e552b57defd8b"
 
   url "https://github.com/swmaeStrong/Pawcus-Public/releases/download/v#{version}/Pawcus.zip"
   name "Pawcus"
